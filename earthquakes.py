7ebf0efba5ed3ac1d856076c0a97b35504269da3
Zhaobo Wang <ucapzw5@ucl.ac.uk>
2024-10-21 10:58:15 +0100

    prof wang first attempt

--- earthquakes.py
+++ earthquakes.py
@@ -1,61 +1,80 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
+from datetime import date
+import matplotlib.pyplot as plt
 import requests
 
-
 def get_data():
-    # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
-    response = requests.get(
-        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
-        params={
-            'starttime': "2000-01-01",
-            "maxlatitude": "58.723",
-            "minlatitude": "50.008",
-            "maxlongitude": "1.67",
-            "minlongitude": "-9.756",
-            "minmagnitude": "1",
-            "endtime": "2018-10-11",
-            "orderby": "time-asc"}
-    )
+    """Retrieve the earthquake data from USGS."""
+    url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-    # The response we get back is an object with several fields.
-    # The actual contents we care about are in its text field:
-    text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
-
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
-
-def count_earthquakes(data):
-    """Get the total number of earthquakes in the response."""
-    return ...
-
+def get_year(earthquake):
+    """Extract the year from the earthquake timestamp."""
+    timestamp = earthquake['properties']['time']
+    year = date.fromtimestamp(timestamp / 1000).year
+    return year
 
 def get_magnitude(earthquake):
-    """Retrive the magnitude of an earthquake item."""
-    return ...
+    """Retrieve the magnitude of an earthquake."""
+    return earthquake['properties']['mag']
 
+def get_magnitudes_per_year(earthquakes):
+    """Retrieve earthquake magnitudes grouped by year."""
+    magnitudes_per_year = {}
+    for quake in earthquakes:
+        year = get_year(quake)
+        magnitude = get_magnitude(quake)
 
-def get_location(earthquake):
-    """Retrieve the latitude and longitude of an earthquake item."""
-    # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+        if magnitude is not None:
+            if year in magnitudes_per_year:
+                magnitudes_per_year[year].append(magnitude)
+            else:
+                magnitudes_per_year[year] = [magnitude]
+    return magnitudes_per_year
 
+def plot_average_magnitude_per_year(earthquakes):
+    """Plot the average magnitude of earthquakes per year."""
+    magnitudes_per_year = get_magnitudes_per_year(earthquakes)
+    years = sorted(magnitudes_per_year.keys())
+    average_magnitudes = [sum(magnitudes_per_year[year]) / len(magnitudes_per_year[year]) for year in years]
 
-def get_maximum(data):
-    """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    plt.figure(figsize=(10, 6))
+    plt.plot(years, average_magnitudes, marker='o', linestyle='-', color='red')
+    plt.title('Average Magnitude of Earthquakes Per Year')
+    plt.xlabel('Year')
+    plt.ylabel('Average Magnitude')
+    plt.grid(True)
+    plt.show()
 
+def plot_number_per_year(earthquakes):
+    """Plot the number of earthquakes per year."""
+    quakes_per_year = {}
+    for quake in earthquakes:
+        year = get_year(quake)
+        if year in quakes_per_year:
+            quakes_per_year[year] += 1
+        else:
+            quakes_per_year[year] = 1
 
-# With all the above functions defined, we can now call them and get the result
-data = get_data()
-print(f"Loaded {count_earthquakes(data)}")
-max_magnitude, max_location = get_maximum(data)
-print(f"The strongest earthquake was at {max_location} with magnitude {max_magnitude}")
+    years = sorted(quakes_per_year.keys())
+    numbers = [quakes_per_year[year] for year in years]
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(years, numbers, color='blue')
+    plt.title('Number of Earthquakes Per Year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Earthquakes')
+    plt.grid(True)
+    plt.show()
+
+# Retrieve and process earthquake data
+quakes = get_data()['features']
+
+# Debugging step: Check if data is loaded correctly
+print(f"Number of earthquakes: {len(quakes)}")
+
+# Generate and show plots
+plot_number_per_year(quakes)
+plt.clf()  # Clear the figure for the next plot
+plot_average_magnitude_per_year(quakes)
